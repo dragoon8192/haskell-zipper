660b8deb6ac217ab1501fb0ce57f763fd64b41b4
dragoon8192_mac <dragoon8192@gmail.com>
2021-04-27 01:06:14 +0900

    fix:src/Zipper.hs

--- src/Zipper.hs
+++ src/Zipper.hs
@@ -1,9 +1,12 @@
-module Zipper where
+module Zipper
+  ( Zipper (MkZipper), left, right
+  ) where
 import Control.Comonad
+import Control.Monad
 import Data.Maybe
 
 data Zipper a = MkZipper [a] a [a]
-  deriving Show
+  deriving (Show, Functor)
 
 left, right :: Zipper a -> Maybe (Zipper a)
 left  (MkZipper [] _ _) = Nothing
@@ -11,15 +14,16 @@ left  (MkZipper (l:ls) c rs) = Just $ MkZipper ls l (c:rs)
 right (MkZipper _ _ []) = Nothing
 right (MkZipper ls c (r:rs)) = Just $ MkZipper (c:ls) r rs
 
-instance Functor Zipper where
-  fmap f (MkZipper ls c rs) = MkZipper (fmap f ls) (f c) (fmap f rs)
-
 instance Comonad Zipper where
   extract (MkZipper _ c _)  = c
-  duplicate z = MkZipper (tail . iterateMaybe left $ z) z (tail . iterateMaybe right $ z)
+  duplicate z = MkZipper ls z rs
+    where
+      ls = tail . iterateMaybe left $ z
+      rs = tail . iterateMaybe right $ z
 
 iterateMaybe :: (a -> Maybe a) -> a -> [a]
 iterateMaybe f = catMaybes . takeWhile isJust . iterateM f . Just
   --Nothing の連続に対応するため takeWhile isJust
-  where iterateM :: (Monad m) => (a -> m a) -> m a -> [m a]
-        iterateM f = iterate (>>=f)
+  where
+    iterateM :: (Monad m) => (a -> m a) -> m a -> [m a]
+    iterateM = iterate . (=<<)
