5a2d521bb197403e5cc76a98a2c76bb4749823ca
dragoon8192_mac <dragoon8192@gmail.com>
2021-05-07 19:44:02 +0900

    update:src/Zipper2.hs use ZipperT

--- src/Zipper2.hs
+++ src/Zipper2.hs
@@ -1,21 +1,25 @@
 module Zipper2 (
-    Zipper2 (MkZipper2), listToZipper2
+    Zipper2, listToZipper2
   ) where
 import Control.Comonad
 import Control.Monad
 import Data.Maybe
 import Data.List
 import Zipper
+import ZipperT
 
 --------------------------
 -- Zipper2
 --------------------------
-newtype Zipper2 a where
-  MkZipper2 :: Zipper (Zipper a) -> Zipper2 a
-  deriving (Functor,Show)
-instance Comonad Zipper2 where
-  extract (MkZipper2 zz) = extract . extract $ zz
-  duplicate (MkZipper2 zz) = fmap MkZipper2 $ MkZipper2 $ extend duplicate zz
+type Zipper2 = ZipperT Zipper
+--newtype Zipper2 a where
+--  MkZipper2 :: Zipper (Zipper a) -> Zipper2 a
+--  deriving (Functor,Show)
+--instance Comonad Zipper2 where
+--  extract (MkZipper2 zz) = extract . extract $ zz
+--  duplicate (MkZipper2 zz) = fmap MkZipper2 $ MkZipper2 $ extend duplicate zz
 
 listToZipper2 :: [[a]] -> Zipper2 a
-listToZipper2 = MkZipper2 . listToZipper . map listToZipper
+listToZipper2 = MkZipperT . listToZipper . map listToZipper
+zipperToList2 :: Zipper2 a -> [[a]]
+zipperToList2 = map zipperToList . zipperToList . runZipperT
--- src/ZipperT.hs
+++ src/ZipperT.hs
@@ -1,29 +1,34 @@
 module ZipperT (
-    ZipperT(MkZipperT), left, right, listToZipper, zipperToList,
+    ZipperT(MkZipperT,runZipperT),
     module Control.Comonad
   ) where
-import Zipper
 import Control.Comonad
+import Control.Comonad.Trans.Class
 import Control.Monad
 import Data.Maybe
 import Data.List
+import Zipper
 
 
 newtype ZipperT w a = MkZipperT {runZipperT :: w (Zipper a)}
   deriving (Functor)
+instance ComonadTrans ZipperT where
+  lower = fmap extract . runZipperT
 instance (Comonad w) => Comonad (ZipperT w) where
   extract = extract . extract . runZipperT
-  duplicate = fmap MkZipperT . MkZipperT . fmap innerDupulicate . duplicate . runZipperT
+  duplicate = fmap MkZipperT . MkZipperT
+              . fmap innerDupulicate . duplicate . runZipperT
 
 innerDupulicate :: (Comonad w) => w (Zipper a) -> Zipper (w (Zipper a))
 innerDupulicate wz = MkZipper ls wz rs
   where
     ls = tail . iterateMaybe leftW $ wz
     rs = tail . iterateMaybe rightW $ wz
+
     leftW, rightW :: (Comonad w) => w (Zipper a) -> Maybe (w (Zipper a))
     leftW = wMb2MbW . fmap left
     rightW= wMb2MbW . fmap right
-    -- MaybeT w ~= w ○ Maybe の w と Maybe を交換
+    -- w ○ Maybe の w と Maybe を交換
     wMb2MbW  :: (Comonad w) => w (Maybe a)-> Maybe(w a)
     wMb2MbW wMbA =
       if isJust . extract $ wMbA
