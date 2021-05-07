4bd3fd17cad4362f2f7ee9eb851dd2be3bbbe879
dragoon8192_mac <dragoon8192@gmail.com>
2021-05-07 21:00:16 +0900

    fix:src/Zipper2.hs

--- src/Zipper2.hs
+++ src/Zipper2.hs
@@ -8,16 +8,7 @@ import Data.List
 import Zipper
 import ZipperT
 
---------------------------
--- Zipper2
---------------------------
 type Zipper2 = ZipperT Zipper
---newtype Zipper2 a where
---  MkZipper2 :: Zipper (Zipper a) -> Zipper2 a
---  deriving (Functor,Show)
---instance Comonad Zipper2 where
---  extract (MkZipper2 zz) = extract . extract $ zz
---  duplicate (MkZipper2 zz) = fmap MkZipper2 $ MkZipper2 $ extend duplicate zz
 
 listToZipper2 :: [[a]] -> Zipper2 a
 listToZipper2 = MkZipperT . listToZipper . map listToZipper
